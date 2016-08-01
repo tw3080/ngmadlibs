104bcfa7409d57565b3d50803adfe6cb9b0d6067
Taylor Welling <tw3080@gmail.com>
2016-08-01 15:18:31 -0700

    added MadLibCtrl to replace app logic

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
-angular.module('app', []);
+angular.module('app', [])
+    .controller('MadLibCtrl', function($scope) {
+        $scope.femaleName = '{{ female name }}';
+        $scope.jobTitle = '{{ job title }}';
+        $scope.tediousTask = '{{ tedious task }}';
+        $scope.dirtyTask = '{{ dirty task }}';
+        $scope.celebrity = '{{ celebrity }}';
+        $scope.uselessSkill = '{{ useless skill }}';
+        $scope.obnoxiousCelebrity = '{{ obnoxious celebrity }}';
+        $scope.hugeNumber = '{{ huge number }}';
+        $scope.adjective = '{{ adjective }}';
+    });
