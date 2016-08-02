1707e451707946dfa72e36805c827b43ff447274
Taylor Welling <tw3080@gmail.com>
2016-08-01 18:20:59 -0700

    MainCtrl constroller is now being used as a class

--- js/app.js
+++ js/app.js
@@ -2,32 +2,32 @@
 TODO: make gender female by default?
 */
 
-angular.module('app', [])
-    .controller('MadLibCtrl', function($scope) {
-        $scope.personName = '{{ name }}';
-        $scope.jobTitle = '{{ job title }}';
-        $scope.tediousTask = '{{ tedious task }}';
-        $scope.dirtyTask = '{{ dirty task }}';
-        $scope.celebrity = '{{ celebrity }}';
-        $scope.uselessSkill = '{{ useless skill }}';
-        $scope.obnoxiousCelebrity = '{{ obnoxious celebrity }}';
-        $scope.hugeNumber = '{{ huge number }}';
-        $scope.adjective = '{{ adjective }}';
-        $scope.genderText = {
-            female: {
-                subjectPronoun: 'she',
-                objectPronoun: 'her',
-                possessivePronoun: 'her'
-            },
-            male: {
-                subjectPronoun: 'he',
-                objectPronoun: 'him',
-                possessivePronoun: 'his'
-            },
-            other: {
-                subjectPronoun: 'they',
-                objectPronoun: 'them',
-                possessivePronoun: 'their'
-            }
-        };
-    });
+angular.module('app', []).controller('MainCtrl', function() {
+    var vm = this;
+    vm.personName = '{{ name }}';
+    vm.jobTitle = '{{ job title }}';
+    vm.tediousTask = '{{ tedious task }}';
+    vm.dirtyTask = '{{ dirty task }}';
+    vm.celebrity = '{{ celebrity }}';
+    vm.uselessSkill = '{{ useless skill }}';
+    vm.obnoxiousCelebrity = '{{ obnoxious celebrity }}';
+    vm.hugeNumber = '{{ huge number }}';
+    vm.adjective = '{{ adjective }}';
+    vm.genderText = {
+        female: {
+            subjectPronoun: 'she',
+            objectPronoun: 'her',
+            possessivePronoun: 'her'
+        },
+        male: {
+            subjectPronoun: 'he',
+            objectPronoun: 'him',
+            possessivePronoun: 'his'
+        },
+        other: {
+            subjectPronoun: 'they',
+            objectPronoun: 'them',
+            possessivePronoun: 'their'
+        }
+    };
+});
