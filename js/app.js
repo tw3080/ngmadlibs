4e38de24452f861d96e662b128fd32703d537b83
Taylor Welling <tw3080@gmail.com>
2016-08-02 18:10:05 -0700

    debugging form validation

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ TODO: make gender other by default?
 */
 
 angular.module('app', [])
-    .controller('MainCtrl', function() {
+    .controller('MainCtrl', function($scope) {
         var vm = this;
         vm.personName = '{{ name }}';
         vm.jobTitle = '{{ job title }}';
@@ -16,6 +16,25 @@ angular.module('app', [])
         vm.adjective = '{{ adjective }}';
         vm.genderText = '';
         vm.showMadLib = false;
+        vm.submit = function() {
+            if ($scope.madLibForm.$valid) {
+                console.log('The form is valid');
+            } else {
+                console.log('The form is invalid');
+            }
+        };
+
+        /* TODO: DIDN'T WORK; why? Had to replace vm with $scope,
+        and add $scope as a parameter of the controller
+        vm.submit = function() {
+            if (vm.madLibForm.$valid) {
+                console.log('The form is valid');
+            } else {
+                console.log('The form is invalid');
+            }
+        };
+        */
+
         /*{
             female: {
                 subjectPronoun: 'she',
